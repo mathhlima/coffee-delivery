80d16ccaaad7a8f9dfac113a4aa9c7c7e04af3d9
Matheus <matheus_sousa@protonmail.com>
2022-07-27 17:19:01 -0300

    add coffe img

--- src/pages/Home/Coffee/index.tsx
+++ src/pages/Home/Coffee/index.tsx
@@ -3,8 +3,11 @@ import coffees from '../../../local-json/coffee.json'
 export function OurCoffee() {
   return (
     <div>
-      {coffees.map(({ name, id }) => (
-        <div key={id}>{name}</div>
+      {coffees.map(({ name, id, type }) => (
+        <div key={id}>
+          {name}
+          {type}
+        </div>
       ))}
     </div>
   )
