62f49f78355dd0ebb3c4fd30ef336ed2075d47cd
Matheus <matheus_sousa@protonmail.com>
2022-07-27 20:20:56 -0300

    css ajustes

--- src/pages/Home/Coffee/index.tsx
+++ src/pages/Home/Coffee/index.tsx
@@ -1,8 +1,21 @@
 import coffees from '../../../local-json/coffee.json'
+import { CardCoffee, CoffeeContainer } from './styles'
 
 export function OurCoffee() {
   return (
-    <h1>Nossos Cafés</h1>
+    <CoffeeContainer>
+      <p>Nossos cafés</p>
+
+      {coffees.map(({ id, name, description, type, img, value }) => (
+        <CardCoffee key={id}>
+          <img src={img} alt="" />
+          {type}
+          {name}
+          {description}
+          R$ {value}
+        </CardCoffee>
+      ))}
+    </CoffeeContainer>
     // <div>
     //   {coffees.map(({ name, id, type, img }) => (
     //     <div key={id}>
--- src/pages/Home/Coffee/styles.ts
+++ src/pages/Home/Coffee/styles.ts
@@ -1,3 +1,16 @@
 import styled from 'styled-components'
 
-export const CoffeeContainer = styled.div``
+export const CoffeeContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  border: 1px solid red;
+`
+
+export const CardCoffee = styled.div`
+  width: 256px;
+  height: 310px;
+  background: ${(props) => props.theme['base-card']};
+  border-radius: 6px 36px;
+  margin-left: 160px;
+  margin-right: 160px;
+`
